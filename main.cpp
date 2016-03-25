21ffa5cb63a19af395e46abe6cb9632244198465
yuhang1211 <957637512@qq.com>
2016-03-25 17:50:24 +0800

    返回整数数组最大子数组和
    返回整数数组最大子数组和

--- main.cpp
+++ main.cpp
@@ -1,69 +1,48 @@
-//随机生成30道四则运算（包括真分数）
-//孔宇航 2016/3/4
+//返回一个整数数组中最大子数组的和
+//孔宇航，赵子茵 2016/3/21
 
 #include<iostream>
-#include<stdlib.h>
-#include<iomanip>
-#include<time.h>
 using namespace std;
-#define random() (rand()%100)
-void main()
+
+int main()
 {
-	   int num1,num2,num3,num4;
-	   srand((int)time(NULL));
-       for(int i=0;i<30;i++)
-	  {
-		 num1=random();
-		 num2=random();
-		int k=(int)rand()%5;
-		switch(k)
-		{
-		case 0:{cout<<num1<<"+"<<num2<<"="; 
-		break;}                                 //进行整数加法运算
-		case 1:{cout<<num1<<"-"<<num2<<"=";
-		break;}                                //进行整数减法运算
-		case 2:{cout<<num1<<"*"<<num2<<"=";
-		break;}                                //进行整数乘法运算
-		case 3:{
-		while(num2==0)
-		num2=random();
-		cout<<num1<<"/"<<num2<<"=";
-		break;}                                //进行整数除法运算
-		case 4:                                //真分数运算部分
-	    {
-          while(num2==0||num1>=num2)
-         {
-            num1=random();
-		    num2=random();
-         }
-            num3=random();
-		    num4=random();
-        while(num4==0||num3>=num4)
-        {
-            num3=random();
-		    num4=random();
-        }
-	   int q=(int)rand()%4;
-        switch(q)
-        {
-            case 0:
-                cout<<"("<<num1<<"/"<<num2<<")"<<"+"<<"("<<num3<<"/"<<num4<<")"<<"=";
-               
-                break;
-            case 1: 
-				cout<<"("<<num1<<"/"<<num2<<")"<<"-"<<"("<<num3<<"/"<<num4<<")"<<"=";
-              
-                break;
-            case 2:
-                 cout<<"("<<num1<<"/"<<num2<<")"<<"*"<<"("<<num3<<"/"<<num4<<")"<<"=";
-               
-                break;
-            case 3:
-                cout<<"("<<num1<<"/"<<num2<<")"<<"/"<<"("<<num3<<"/"<<num4<<")"<<"=";
-                break;
-		   } 
-		  }
-        }
-		cout<<endl;
-	  }
+    int arr[1000];
+    int i, MaxSum = 0, max, sum = 0, num = 1, count = 0;
+    /*MaxSum是子数组最大和
+      max是整数组的最大值
+      sum是子数组的和
+      num是元素个数
+      count是元素中负数的个数*/
+    cout << "请输入一个整数数组（不超过1000个）：" << endl;
+    cin >> arr[0];
+    while (getchar() != '\n')
+    {
+        cin >> arr[num];
+        num++;
+    }
+
+    for (i = 0; i < num; i++)
+    {
+        if (sum < 0)
+            sum = arr[i];//当子数组和小于0时，与无论与后面数组如何相加，和肯定小于后一段数组之和，此时，将b重新赋值，置为下一个元素
+        else
+            sum += arr[i];
+        if (MaxSum < sum)
+            MaxSum = sum;
+    }
+
+    max = arr[0];//d记录数组中的最大元素值
+    for (i = 0; i<num; i++)  //如果数组全为负数，最大子数组和就是数组中最大值
+    {
+        if (arr[i]<0)
+            count++;
+        if (arr[i]>max)
+            max = arr[i];
+        if (count == num)
+            MaxSum = max;
+
+    }
+    cout << "子数组的最大和为：" << MaxSum << endl;
+    return 0;
+
 }
